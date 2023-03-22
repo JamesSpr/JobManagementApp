047b13ca9fa51bfff597798cc1f2f05924d97b43
James Sprague <jamessprague07@gmail.com>
2023-03-22 15:11:23 +1100

    Updated models

--- project_management/analytics/models.py
+++ project_management/analytics/models.py
@@ -8,6 +8,7 @@ class Sync(models.Model):
     MODEL_CHOICES = [
         ('ACC', 'Account'),
         ('TRA', 'Transaction'),
+        ('CLI', 'Clients')
     ]
 
     id = models.AutoField(primary_key=True, unique=True)
@@ -17,24 +18,27 @@ class Sync(models.Model):
 
 class Account(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
-    myob_uid = models.CharField(max_length=36, blank=True)
+    myob_uid = models.CharField(max_length=36)
     display_id = models.CharField(max_length=12)
-    name = models.CharField(max_length=60, blank=True, null=True)
+    name = models.CharField(max_length=60)
     level = models.IntegerField()
     current_balance = models.DecimalField(max_digits=13, default='0.00', decimal_places=2)
 
 
+class Client(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    myob_uid = models.CharField(max_length=36)
+
 class Transaction(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
-    myob_uid = models.CharField(max_length=36, blank=True)
+    myob_uid = models.CharField(max_length=36)
     display_id = models.CharField(max_length=12)
-    description = models.CharField(max_length=255, blank=True, null=True)
-    journal_type = models.CharField(max_length=255, blank=True, null=True)
+    description = models.CharField(max_length=255, blank=True)
+    journal_type = models.CharField(max_length=255, blank=True)
     account = models.ForeignKey(Account, on_delete=models.PROTECT)
     amount = models.DecimalField(max_digits=13, default='0.00', decimal_places=2)
     is_credit = models.BooleanField()
     job_uid = models.CharField(max_length=36, blank=True)
-    job_number = models.CharField(max_length=30, blank=True)
     source_transaction_type = models.CharField(max_length=30, blank=True)
     date_occurred = models.DateField()
 
