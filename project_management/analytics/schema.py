d386c02e28ba78027343b3c28740477b2c3cda9b
James Sprague <jamessprague07@gmail.com>
2023-03-22 18:31:43 +1100

    Added Schema Mutations

--- project_management/analytics/schema.py
+++ project_management/analytics/schema.py
@@ -2,6 +2,7 @@ import graphene
 from graphene_django import DjangoObjectType
 from graphene import relay
 from graphql_jwt.decorators import login_required
+from .models import Account, Transaction
 
 class AccountInputType(graphene.InputObjectType):
     myob_uid = graphene.String()
@@ -18,6 +19,17 @@ class UpdateAccounts(graphene.Mutation):
 
     @login_required
     def mutate(self, root, info, accounts):
+        for acc in accounts:
+            if Account.objects.filter(myob_uid=acc.myob_uid):
+                account = Account.object.get(myob_uid=acc.myob_uid)
+            else:
+                account = Account()
+
+            account.display_id = acc.display_id
+            account.name = acc.name
+            account.level = acc.level
+            account.current_balance = acc.current_balance
+            account.save()
 
         return self(success=True)
 
@@ -30,8 +42,8 @@ class TransactionInputType(graphene.InputObjectType):
     amount = graphene.Float()
     is_credit = graphene.Boolean()
     job_uid = graphene.String()
-    job_number = graphene.String()
     source_transaction_type = graphene.String()
+    date_occurred = graphene.Date()
 
 class UpdateTransactions(graphene.Mutation):
     class Arguments:
@@ -41,6 +53,23 @@ class UpdateTransactions(graphene.Mutation):
 
     @login_required
     def mutate(self, root, info, transactions):
+        for trans in transactions:
+            if Transaction.objects.filter(myob_uid=trans.myob_uid):
+                transaction = Transaction.object.get(myob_uid=trans.myob_uid)
+            else:
+                transaction = Transaction()
+        
+            transaction.myob_uid = trans.myob_uid
+            transaction.display_id = trans.display_id
+            transaction.description = trans.description
+            transaction.journal_type = trans.journal_type
+            transaction.account = trans.account
+            transaction.amount = trans.amount
+            transaction.is_credit = trans.is_credit
+            transaction.job_uid = trans.job_uid
+            transaction.source_transaction_type = trans.source_transaction_type
+            transaction.date_occurred = trans.date_occurred
+            transaction.save()
 
         return self(success=False)
 
